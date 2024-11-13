f85d0f75cab9db80450520f5f85daf1762f90612
azure0929 <chary8028@naver.com>
2024-11-13 09:41:48 +0900

    feat: refactor: 상품 정보 동적으로 렌더링

---
+++ src/data/products.js
@@ -0,0 +1,74 @@
+const products = [
+  {
+    id: 1,
+    name: "카누 바리스타 머신 어반 스노우 화이트",
+    price: "199,000원",
+    image:
+      "https://kanu.co.kr/web/product/medium/202410/a249921bb55e07c7ed74a21578efe14a.png",
+  },
+  {
+    id: 2,
+    name: "카누 바리스타 머신 어반 오로라 블랙",
+    price: "199,000원",
+    image:
+      "https://kanu.co.kr/web/product/medium/202410/7fe131241ded1be5284db63796c257d3.png",
+  },
+  {
+    id: 3,
+    name: "카누 바리스타 머신 어반 차콜 브라운",
+    price: "199,000원",
+    image:
+      "https://kanu.co.kr/web/product/medium/202410/28f4a0e10a3e15fdd5307315bfae9e39.png",
+  },
+  {
+    id: 4,
+    name: "카누 바리스타 머신 브리즈 파스텔 핑크",
+    price: "169,000원",
+    image:
+      "https://kanu.co.kr/web/product/medium/202410/fb30f229d13a5abedf38e556a98fa08f.png",
+  },
+  {
+    id: 5,
+    name: "카누 바리스타 머신 브리즈 캐슬그레이",
+    price: "169,000원",
+    image:
+      "https://kanu.co.kr/web/product/medium/202410/d5756d6ab39a81ceac60b21091538ee5.png",
+  },
+  {
+    id: 6,
+    name: "카누 바리스타 머신 브리즈 파스텔 블루",
+    price: "169,000원",
+    image:
+      "https://kanu.co.kr/web/product/medium/202410/343ca405aa2892075d80491eeb901b2d.png",
+  },
+  {
+    id: 7,
+    name: "카누 바리스타 머신 브리즈 샌드 베이지",
+    price: "169,000원",
+    image:
+      "https://kanu.co.kr/web/product/medium/202410/87f5c92fdbcbf9a0dd5fc718169ed4c8.png",
+  },
+  {
+    id: 8,
+    name: "카누 바리스타 머신 브리즈 크림 화이트",
+    price: "169,000원",
+    image:
+      "https://kanu.co.kr/web/product/medium/202410/8a14c71854cafbb7852cf0abd8f9e40a.png",
+  },
+  {
+    id: 9,
+    name: "카누 바리스타 머신 페블 블랙",
+    price: "149,000원",
+    image:
+      "https://kanu.co.kr/web/product/medium/202410/4885199f1698e403f33802a3d1933718.png",
+  },
+  {
+    id: 10,
+    name: "카누 바리스타 머신 페블 화이트",
+    price: "149,000원",
+    image:
+      "https://kanu.co.kr/web/product/medium/202410/19f6d000782d7cf1fa3db11dfb7aa299.png",
+  },
+];
+
+export default products;
--- src/routes/Shop.js
+++ src/routes/Shop.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import products from "../data/products";
 import "./Shop.css";
 import "../App.css";
 
@@ -47,262 +48,29 @@ function Shop() {
           </div>
           <div className="items">
             <ul>
-              <li className="item">
-                <div className="item-content">
-                  <div className="thumbnail">
-                    <a href="#none">
-                      <img
-                        src="https://kanu.co.kr/web/product/medium/202410/a249921bb55e07c7ed74a21578efe14a.png"
-                        alt="카누 바리스타 머신 어반 스노우 화이트"
-                      />
-                    </a>
-                  </div>
-                  <div className="addCartItem">
-                    <div></div>
-                    장바구니 담기
-                  </div>
-                  <div className="product-detail">
-                    <div>
-                      <p className="cnb-name">
-                        카누 바리스타 머신 어반 스노우 화이트
-                      </p>
+              {products.map((product) => (
+                <li key={product.id} className="item">
+                  <div className="item-content">
+                    <div className="thumbnail">
+                      <a href="#none">
+                        <img src={product.image} alt={product.name} />
+                      </a>
                     </div>
-                    <div className="price">
-                      <span>199,000원</span>
+                    <div className="addCartItem">
+                      <div></div>
+                      장바구니 담기
+                    </div>
+                    <div className="product-detail">
+                      <div>
+                        <p className="cnb-name">{product.name}</p>
+                      </div>
+                      <div className="price">
+                        <span>{product.price}</span>
+                      </div>
                     </div>
                   </div>
-                </div>
-              </li>
-              <li className="item">
-                <div className="item-content">
-                  <div className="thumbnail">
-                    <a href="#none">
-                      <img
-                        src="https://kanu.co.kr/web/product/medium/202410/7fe131241ded1be5284db63796c257d3.png"
-                        alt="카누 바리스타 머신 어반 오로라 블랙"
-                      />
-                    </a>
-                  </div>
-                  <div className="addCartItem">
-                    <div></div>
-                    장바구니 담기
-                  </div>
-                  <div className="product-detail">
-                    <div>
-                      <p className="cnb-name">
-                        카누 바리스타 머신 어반 오로라 블랙
-                      </p>
-                    </div>
-                    <div className="price">
-                      <span>199,000원</span>
-                    </div>
-                  </div>
-                </div>
-              </li>
-              <li className="item">
-                <div className="item-content">
-                  <div className="thumbnail">
-                    <a href="#none">
-                      <img
-                        src="https://kanu.co.kr/web/product/medium/202410/28f4a0e10a3e15fdd5307315bfae9e39.png"
-                        alt="카누 바리스타 머신 어반 차콜 브라운"
-                      />
-                    </a>
-                  </div>
-                  <div className="addCartItem">
-                    <div></div>
-                    장바구니 담기
-                  </div>
-                  <div className="product-detail">
-                    <div>
-                      <p className="cnb-name">
-                        카누 바리스타 머신 어반 차콜 브라운
-                      </p>
-                    </div>
-                    <div className="price">
-                      <span>199,000원</span>
-                    </div>
-                  </div>
-                </div>
-              </li>
-              <li className="item">
-                <div className="item-content">
-                  <div className="thumbnail">
-                    <a href="#none">
-                      <img
-                        src="https://kanu.co.kr/web/product/medium/202410/fb30f229d13a5abedf38e556a98fa08f.png"
-                        alt="카누 바리스타 머신 브리즈 파스텔 핑크"
-                      />
-                    </a>
-                  </div>
-                  <div className="addCartItem">
-                    <div></div>
-                    장바구니 담기
-                  </div>
-                  <div className="product-detail">
-                    <div>
-                      <p className="cnb-name">
-                        카누 바리스타 머신 브리즈 파스텔 핑크
-                      </p>
-                    </div>
-                    <div className="price">
-                      <span>169,000원</span>
-                    </div>
-                  </div>
-                </div>
-              </li>
-              <li className="item">
-                <div className="item-content">
-                  <div className="thumbnail">
-                    <a href="#none">
-                      <img
-                        src="https://kanu.co.kr/web/product/medium/202410/d5756d6ab39a81ceac60b21091538ee5.png"
-                        alt="카누 바리스타 머신 브리즈 캐슬그레이"
-                      />
-                    </a>
-                  </div>
-                  <div className="addCartItem">
-                    <div></div>
-                    장바구니 담기
-                  </div>
-                  <div className="product-detail">
-                    <div>
-                      <p className="cnb-name">
-                        카누 바리스타 머신 브리즈 캐슬그레이
-                      </p>
-                    </div>
-                    <div className="price">
-                      <span>169,000원</span>
-                    </div>
-                  </div>
-                </div>
-              </li>
-              <li className="item">
-                <div className="item-content">
-                  <div className="thumbnail">
-                    <a href="#none">
-                      <img
-                        src="https://kanu.co.kr/web/product/medium/202410/343ca405aa2892075d80491eeb901b2d.png"
-                        alt="카누 바리스타 머신 브리즈 파스텔 블루"
-                      />
-                    </a>
-                  </div>
-                  <div className="addCartItem">
-                    <div></div>
-                    장바구니 담기
-                  </div>
-                  <div className="product-detail">
-                    <div>
-                      <p className="cnb-name">
-                        카누 바리스타 머신 브리즈 파스텔 블루
-                      </p>
-                    </div>
-                    <div className="price">
-                      <span>169,000원</span>
-                    </div>
-                  </div>
-                </div>
-              </li>
-              <li className="item">
-                <div className="item-content">
-                  <div className="thumbnail">
-                    <a href="#none">
-                      <img
-                        src="https://kanu.co.kr/web/product/medium/202410/87f5c92fdbcbf9a0dd5fc718169ed4c8.png"
-                        alt="카누 바리스타 머신 브리즈 샌드 베이지"
-                      />
-                    </a>
-                  </div>
-                  <div className="addCartItem">
-                    <div></div>
-                    장바구니 담기
-                  </div>
-                  <div className="product-detail">
-                    <div>
-                      <p className="cnb-name">
-                        카누 바리스타 머신 브리즈 샌드 베이지
-                      </p>
-                    </div>
-                    <div className="price">
-                      <span>169,000원</span>
-                    </div>
-                  </div>
-                </div>
-              </li>
-              <li className="item">
-                <div className="item-content">
-                  <div className="thumbnail">
-                    <a href="#none">
-                      <img
-                        src="https://kanu.co.kr/web/product/medium/202410/8a14c71854cafbb7852cf0abd8f9e40a.png"
-                        alt="카누 바리스타 머신 브리즈 크림 화이트"
-                      />
-                    </a>
-                  </div>
-                  <div className="addCartItem">
-                    <div></div>
-                    장바구니 담기
-                  </div>
-                  <div className="product-detail">
-                    <div>
-                      <p className="cnb-name">
-                        카누 바리스타 머신 브리즈 크림 화이트
-                      </p>
-                    </div>
-                    <div className="price">
-                      <span>169,000원</span>
-                    </div>
-                  </div>
-                </div>
-              </li>
-              <li className="item">
-                <div className="item-content">
-                  <div className="thumbnail">
-                    <a href="#none">
-                      <img
-                        src="https://kanu.co.kr/web/product/medium/202410/4885199f1698e403f33802a3d1933718.png"
-                        alt="카누 바리스타 머신 페블 블랙"
-                      />
-                    </a>
-                  </div>
-                  <div className="addCartItem">
-                    <div></div>
-                    장바구니 담기
-                  </div>
-                  <div className="product-detail">
-                    <div>
-                      <p className="cnb-name">카누 바리스타 머신 페블 블랙</p>
-                    </div>
-                    <div className="price">
-                      <span>149,000원</span>
-                    </div>
-                  </div>
-                </div>
-              </li>
-              <li className="item">
-                <div className="item-content">
-                  <div className="thumbnail">
-                    <a href="#none">
-                      <img
-                        src="https://kanu.co.kr/web/product/medium/202410/19f6d000782d7cf1fa3db11dfb7aa299.png"
-                        alt="카누 바리스타 머신 페블 화이트"
-                      />
-                    </a>
-                  </div>
-                  <div className="addCartItem">
-                    <div></div>
-                    장바구니 담기
-                  </div>
-                  <div className="product-detail">
-                    <div>
-                      <p className="cnb-name">카누 바리스타 머신 페블 화이트</p>
-                    </div>
-                    <div className="price">
-                      <span>149,000원</span>
-                    </div>
-                  </div>
-                </div>
-              </li>
+                </li>
+              ))}
             </ul>
           </div>
         </div>
